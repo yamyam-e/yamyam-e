22678c80dbd1747bf60f372510f2764ef06e8c98
yamyam-e <yamyam-e@yamyam-e.com>
2017-01-04 06:53:21 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YamyamE::Application.config.secret_key_base = '93cd32f9544874c46917d52dc2b5185673727de45589fc043fe4960e343d639e9da332b2f8710c98927753a2e60da91acb9f0e3541c4df0355e2fecc302d3844'
